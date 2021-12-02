84db3c77b120584d2cf3c4317fe32db037ad7958
Mikhail Romanov <w38@bk.ru>
2021-12-02 12:19:31 +0300

    Updates

---
+++ precache-manifest.17ea045cd56a81c6437cd16bcd2575be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "246d17118944dce1a942eac7b0f8c23d",
+    "url": "/SimpleTasks/index.html"
+  },
+  {
+    "revision": "2a7c4fe22aa380bcd7cb",
+    "url": "/SimpleTasks/static/css/main.6664728f.chunk.css"
+  },
+  {
+    "revision": "f2c477f9633754e00a1e",
+    "url": "/SimpleTasks/static/js/2.ee6fe0dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SimpleTasks/static/js/2.ee6fe0dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a7c4fe22aa380bcd7cb",
+    "url": "/SimpleTasks/static/js/main.9c4ab38e.chunk.js"
+  },
+  {
+    "revision": "f5a2bf9217223f3173a7",
+    "url": "/SimpleTasks/static/js/runtime-main.4840aeeb.js"
+  }
+]);
